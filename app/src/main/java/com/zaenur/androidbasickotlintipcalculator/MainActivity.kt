19975085ee4cf4c736e6a864e97a004bc19f0ae1
rochman25 <zaenur.rochman98@gmail.com>
2022-03-30 09:12:01 +0700

    init project

---
+++ app/src/main/java/com/zaenur/androidbasickotlintipcalculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zaenur.androidbasickotlintipcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
